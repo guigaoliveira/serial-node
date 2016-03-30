c6bde9d80b615035b2ddb0587169fbc0c84459d7
Guilherme Matheus Borges de Oliveira <gmbo182@hotmail.com>
2016-03-30 16:20:03 -0300

    Update app.js

--- exemples/app.js
+++ exemples/app.js
@@ -3,11 +3,8 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var serial= require('./serial-node');
-<<<<<<< HEAD:app.js
 app.use(express.static(__dirname + '/exemples/web'));
-=======
 app.use(express.static(__dirname + '/web'));
->>>>>>> origin/master:exemples/app.js
 server.listen(80, function() {console.log("Server Online");});
 io.on('connection', function (socket) 
 {  
